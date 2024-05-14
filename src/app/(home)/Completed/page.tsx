cceb909e1303d32fce984f26e1489e9d0f4fd25b
Harii <hlshariom@gmail.com>
2024-05-15 01:06:55 +0530

    #lengtherror

--- src/app/(home)/Completed/page.tsx
+++ src/app/(home)/Completed/page.tsx
@@ -31,7 +31,7 @@ export default function Completed(){
     return (
         <div className="absolute top-24 w-full  grid grid-flow-row justify-items-center gap-y-5">
             <div className="absolute w-full grid grid-flow-row justify-center overflow-y-scroll gap-y-4 pb-[195px] pt-5">
-                {tasks.length>0?({tasks}.tasks.map((task,index)=><Taskcard key={index} task={task}/>)):"There is No Task"}
+                {tasks.length>0?(tasks.map((task,index)=><Taskcard key={index} task={task}/>)):"There is No Task"}
         </div>
 
         </div>
