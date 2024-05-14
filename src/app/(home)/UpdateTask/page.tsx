bcf779f09dfa2f9f9abba075f78cd3602dc5497c
Harii <hlshariom@gmail.com>
2024-05-15 01:02:19 +0530

    #suspense error

--- src/app/(home)/UpdateTask/page.tsx
+++ src/app/(home)/UpdateTask/page.tsx
@@ -1,47 +1,12 @@
-'use client'
-import TaskForm from "@/components/shadcn/TaskForm";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
-import axios from "axios";
+import Update from '@/components/shadcn/Update';
+import { Suspense } from 'react';
 
-type Task ={
-    _id:string,
-    taskName:string,
-    taskDescription:string,
-    taskPriority:string,
-    taskDueDate:string,
-    taskStatus:string
-}
-
-export default function Update() {
+function UpdatePage() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <UpdateTask />
+            <Update />
         </Suspense>
     );
 }
 
-function UpdateTask() {
-    const search  = useSearchParams();
-    const [task, setTask] = useState<Task>()
-
-    const updateTask = async() => {
-        try {
-            const res = await axios.get(`/api/Task/Update?id=${search.get("id")}`);
-            setTask(res.data.task);
-        } catch (error:any) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        updateTask();
-    }, []);
-
-    return (
-        <div className="absolute top-24 w-full  grid grid-flow-row justify-items-center gap-y-5">
-            <h1 className="text-4xl font-bold ">Update Task</h1>
-            <TaskForm post={task} action="Update"/>
-        </div>
-    );
-}
+export default UpdatePage;
